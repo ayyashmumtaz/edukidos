3d396d1df0b58cc2fd9f9d7302a6250453873fec
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2022-06-13 09:53:09 +0700

    add view produksi

--- application/controllers/Produksi.php
+++ application/controllers/Produksi.php
@@ -16,46 +16,46 @@ class Produksi extends CI_Controller {
 
 	public function a3()
 	{
-	$data['orderMasuk'] = $this->Model_produksi->getReadya3()->result();;
+	$data['orderMasuk'] = $this->Model_produksi->getCetakA3()->result();;
 	$this->load->view('dashboard/_partials/header');
 	$this->load->view('dashboard/_partials/sidebar');
-	$this->load->view('spk/a3', $data);				
+	$this->load->view('produksi/a3', $data);				
 	$this->load->view('dashboard/_partials/footer');
 	}
 
 	public function indoor()
 	{
-	$data['orderMasuk'] = $this->Model_produksi->getReadyIndoor()->result();;
+	$data['orderMasuk'] = $this->Model_produksi->getCetakIndoor()->result();;
 	$this->load->view('dashboard/_partials/header');
 	$this->load->view('dashboard/_partials/sidebar');
-	$this->load->view('spk/indoor', $data);				
+	$this->load->view('produksi/indoor', $data);				
 	$this->load->view('dashboard/_partials/footer');
 	}
 
 	public function outdoor()
 	{
-	$data['orderMasuk'] = $this->Model_produksi->getReadyOutdoor()->result();;
+	$data['orderMasuk'] = $this->Model_produksi->getCetakOutdoor()->result();;
 	$this->load->view('dashboard/_partials/header');
 	$this->load->view('dashboard/_partials/sidebar');
-	$this->load->view('spk/outdoor', $data);				
+	$this->load->view('produksi/outdoor', $data);				
 	$this->load->view('dashboard/_partials/footer');
 	}
 
 	public function cutting()
 	{
-	$data['orderMasuk'] = $this->Model_produksi->getReadyCutting()->result();;
+	$data['orderMasuk'] = $this->Model_produksi->getCetakCutting()->result();;
 	$this->load->view('dashboard/_partials/header');
 	$this->load->view('dashboard/_partials/sidebar');
-	$this->load->view('spk/cutting', $data);				
+	$this->load->view('produksi/cutting', $data);				
 	$this->load->view('dashboard/_partials/footer');
 	}
 
 	public function finishing()
 	{
-	$data['orderMasuk'] = $this->Model_produksi->getReadyFinishing()->result();;
+	$data['orderMasuk'] = $this->Model_produksi->getCetakFinishing()->result();;
 	$this->load->view('dashboard/_partials/header');
 	$this->load->view('dashboard/_partials/sidebar');
-	$this->load->view('spk/finishing', $data);				
+	$this->load->view('produksi/finishing', $data);				
 	$this->load->view('dashboard/_partials/footer');
 	}
 
--- application/models/Model_produksi.php
+++ application/models/Model_produksi.php
@@ -1,9 +1,42 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ModelName extends CI_Model {
+class Model_produksi extends CI_Model {
 
-	
+	public function getCetakA3()
+	{
+	$kategori = array('kategori' => 'a3','status' => 1);
+	$this->db->where($kategori);
+	return $this->db->get('orderan');
+}
+
+public function getCetakIndoor()
+	{
+	$kategori = array('kategori' => 'indoor','status' => 1);
+	$this->db->where($kategori);
+	return $this->db->get('orderan');
+}
+
+public function getCetakOutdoor()
+	{
+	$kategori = array('kategori' => 'outdoor','status' => 1);
+	$this->db->where($kategori);
+	return $this->db->get('orderan');
+}
+
+public function getCetakCutting()
+	{
+	$kategori = array('kategori' => 'cutting','status' => 1);
+	$this->db->where($kategori);
+	return $this->db->get('orderan');
+}
+
+public function getCetakFinishing()
+	{
+	$kategori = array('kategori' => 'finishing','status' => 1);
+	$this->db->where($kategori);
+	return $this->db->get('orderan');
+}
 
 }
 
