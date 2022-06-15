74c863d6e596f4b86b0461fd5b853f7ef88e6597
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2022-06-16 00:56:32 +0700

    rekap controller

---
+++ application/controllers/Rekap.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rekap extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Rekap.php */
+/* Location: ./application/controllers/Rekap.php */
