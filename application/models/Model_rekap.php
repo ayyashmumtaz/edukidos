40ae7eb5c561d02b70a69d26b4caf80e7482940d
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2022-06-18 15:18:56 +0700

    model_rekap

---
+++ application/models/Model_rekap.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_rekap extends CI_Model {
+
+	
+
+}
+
+/* End of file Model_rekap.php */
+/* Location: ./application/models/Model_rekap.php */
