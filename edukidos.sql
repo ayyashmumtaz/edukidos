f70ec6a34cd0643aed05fec3432b2eb8fe4d4fad
SulthanRaghib <raghib.smp@gmail.com>
2022-10-12 13:22:01 +0700

    update database 12/10/2022

--- edukidos.sql
+++ edukidos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2022 at 05:56 AM
+-- Generation Time: Oct 12, 2022 at 08:19 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.5
 
