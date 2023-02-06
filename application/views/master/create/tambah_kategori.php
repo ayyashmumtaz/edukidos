7bd160fba9920e932321f9e6fe0a163b5e351bf9
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2023-02-06 08:37:22 +0700

    fix rename nama kategori

--- application/views/master/create/tambah_kategori.php
+++ application/views/master/create/tambah_kategori.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<hr>
 	<div class="col-12 d-flex flex-row align-items-center justify-content-between pl-0">
-		<h5 class="mb-0">Tambah Data Bahan</h5>
+		<h5 class="mb-0">Tambah Kategori</h5>
 		<button type="button" class="btn btn-sm btn-warning float-right" data-toggle="modal" data-target="#batal">
 			<i class="fa-solid fa-xmark pr-2"></i>Batal
 		</button>
