d5343e4f64ef5114b607ee4ef34a4bede708549a
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2023-02-14 10:52:44 +0700

    add revisi stok to sidebar for gudang role

--- application/views/dashboard/_partials/sidebar.php
+++ application/views/dashboard/_partials/sidebar.php
@@ -120,8 +120,10 @@
 						<div class="bg-white py-2 collapse-inner rounded">
 
 							<a class="collapse-item" href="<?= site_url('Beranda/stok'); ?>">Data Stok</a>
-							<a class="collapse-item" href="<?= site_url('Gudang/barang_masuk'); ?>">Barang Masuk</a>
-							<a class="collapse-item" href="<?= site_url('Gudang/barang_keluar'); ?>">Barang Keluar</a>
+							<a class="collapse-item" href="<?= site_url('Gudang/revisi_stok'); ?>">Revisi Stok</a>
+							<a class="collapse-item" href="<?= site_url('Gudang/admin_barang_keluar'); ?>">Permintaan</a>
+							<a class="collapse-item" href="<?= site_url('Gudang/barang_masuk'); ?>">Riwayat Barang Masuk</a>
+							<a class="collapse-item" href="<?= site_url('Gudang/riwayat_barang_keluar'); ?>">Riwayat Barang Keluar</a>
 							<a class="collapse-item" href="<?= site_url('Gudang/barang_retur'); ?>">Retur Barang</a>
 
 						</div>
