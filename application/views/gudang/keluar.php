2119812dabf81006646ae101c2fd46d497281385
Ayyash Mumtaz Hafidz <ayyashmumtaz2003@gmail.com>
2023-02-16 13:45:40 +0700

    fix pengiriman barang

--- application/views/gudang/keluar.php
+++ application/views/gudang/keluar.php
@@ -18,6 +18,7 @@
           <th>Tanggal</th>
           <th>Nama User</th>
           <th>Status ?</th> 
+          <th>Aksi</th>
         </tr>
       </thead>
       <tbody>
@@ -29,17 +30,30 @@
             <td><?= $rbk->tanggal_req ?></td>
             <td><?= $rbk->nama ?></td> 
             <?php switch ($rbk->status_barang) {
+              case 'dikirim':
+                echo '<td>';
+                echo '<a href="#" class="btn btn-sm btn-success"><i class="fas fa-check"></i> Barang Sudah Dikirim</a>';
+                echo '</td>';
+                break;
               case 'diterima':
                 echo '<td>';
-                echo '<a href="#" class="btn btn-sm btn-success"><i class="fas fa-check-circle"></i> Diterima</a>';
+                echo '<a href="#" class="btn btn-sm btn-warning"><i class="fas fa-clock"></i> Menunggu Pengiriman Barang</a>';
+                echo '</td>';
+                echo '<td>';
+                echo '<a href="'.base_url().'Gudang/status_dikirim_req_barangkeluar/'.$rbk->id_request.'" onclick="return confirm(`Anda Yakin Ingin Menerima Request Produk : '.$rbk->nama_produk.' Ini?`);" class="btn btn-sm btn-success"><i class="fas fa-check"></i> Kirim Barang</a> ';
                 echo '</td>';
                 break;
               case 'ditolak':
                 echo '<td>';
                 echo '<a href="#" class="btn btn-sm btn-secondary"><i class="fas fa-times-circle"></i> Ditolak</a>';
                 echo '</td>';
+                echo '<td>';
+                echo '</td>';
                 break;
               case 'pending':
+                echo '<td>';
+                echo '<a href="#" class="btn btn-sm btn-warning"><i class="fas fa-clock"></i> Pending</a>';
+                echo '</td>';
                 echo '<td>';
                 echo '<a href="'.base_url().'Gudang/status_diterima_req_barangkeluar/'.$rbk->id_request.'" onclick="return confirm(`Anda Yakin Ingin Menerima Request Produk : '.$rbk->nama_produk.' Ini?`);" class="btn btn-sm btn-success"><i class="fas fa-check"></i> Terima</a> ';
                 echo '<a href="'.base_url().'Gudang/status_ditolak_req_barangkeluar/'.$rbk->id_request.'" onclick="return confirm(`Anda Yakin Ingin Menolak Request Produk : '.$rbk->nama_produk.' Ini?`);"class="btn btn-sm btn-danger"><i class="fas fa-times"></i> Tolak</a>';
